e61c65f6e4b8b6948fa977826ff69601e925b0db
Mikael Teixeira <142433000+MikaelTeixeira@users.noreply.github.com>
2025-06-10 21:13:46 -0300

    Não sei mais como nomear

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from contact import models
 # Register your models here.
+@admin.register(models.Contact)
+class Contact_admin(admin.ModelAdmin):
+    list_display = 'first_name','last_name','phone','email'
--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Contact(models.Model):
+    '''This class represents a contact that will be saved in the database. These fields will be used to create the contacts, and will exist in every contact.'''
+  
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50, blank=True)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254, blank= True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(blank=True)
+    picture = models.ImageField(upload_to='pictures/%Y/%m/', blank=True)
+
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
+        
