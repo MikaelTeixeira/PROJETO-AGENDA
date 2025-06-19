1b1dee06c7029f30f33bc36edf97229787e6eb0b
Mikael Teixeira <142433000+MikaelTeixeira@users.noreply.github.com>
2025-06-19 01:16:59 -0300

    Adicionando paginações
    Basicamente este commit foi focado em adicionar paginações na tabela e nas buscas. Adicionei as paginações e adicionei uma forma de manter as paginações de forma consistente após a busca de algum valor.

--- contact/views.py
+++ contact/views.py
@@ -1,14 +1,21 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from contact.models import Contact
+from django.db.models import Q 
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 
 def index(request):
-    contacts = Contact.objects.filter(show=True).order_by('id')[:10]
+    contacts = Contact.objects.filter(show=True).order_by('id')
+
+    paginator = Paginator(contacts,25)
+    page_number = request.GET.get('page')
+    page_object = paginator.get_page(page_number)
 
     context = {
-        'contacts': contacts,
+        'page_object': page_object,
+        'search_title': 'Contacts',
     }
 
     return render(
@@ -17,10 +24,41 @@ def index(request):
         )
 
 def contact(request, contact_id):
-    single_contact = get_object_or_404(Contact.objects, pk=contact_id)
+    single_contact = get_object_or_404(Contact, pk=contact_id)
 
     context = {
         'contact': single_contact,
+        'page_object': None,
     }
 
     return render( request, 'contact/contact.html', context)
+
+
+def search(request):
+
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == '':
+        return redirect('contact:index')
+
+    contacts = Contact.objects\
+        .filter(show=True)\
+        .filter( 
+            Q(first_name__icontains=search_value) | 
+            Q(last_name__icontains=search_value)
+                )\
+        .order_by('id')
+
+    paginator = Paginator(contacts,25)
+    page_number = request.GET.get('page')
+    page_object = paginator.get_page(page_number)
+    context = {
+        'page_object': page_object,
+        'search_value': search_value
+    }
+
+    return render(
+        request,
+        'contact/index.html',
+        context
+    )
